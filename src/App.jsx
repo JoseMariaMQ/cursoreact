b8eb8b2206a18582d610755aef12ef294613840b
José María Muñoz Quijada <JoseMariaMQ@users.noreply.github.com>
2021-02-16 17:48:20 +0100

    List Key Map

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Contador from './components/Contador'
+import Listado from "./components/Listado";
 
 function App() {
   return (
     <div className="App">
       <h1>Título de nuestro primer proyecto</h1>
-        <Contador></Contador>
-        <Contador></Contador>
-        <Contador></Contador>
-        <Contador></Contador>
+      <Listado></Listado>
     </div>
   );
 }
